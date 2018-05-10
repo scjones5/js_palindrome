7789fcb475ad03ff723af4d75c0b25e6d41ed879
scjones5 <sjones.uleth@gmail.com>
2018-05-10 10:36:50 -0400

    Initialize repository

---
+++ index.js
@@ -0,0 +1,32 @@
+// Adds `reverse` to all strings.
+String.prototype.reverse = function() {
+	return Array.from(this).reverse().join("");
+}
+
+// Defines a Phrase object.
+function Phrase(content) {
+	this.content = content;
+
+	// Returns content processed for palindrome testing.
+	this.processedContent = function processedContent() {
+		return this.content.toLowerCase();
+	}
+
+	// Returns true for a palindrome, false otherwise.
+	this.palindrome = function palindrome() {
+        	return this.processedContent() === this.processedContent().reverse();
+	}
+} 
+
+// Defined a translated Phrase object.
+function TranslatedPhrase(content, translation) {
+	this.content = content;
+	this.translation = translation;
+
+	// Returns translation processed for palindrome testing.
+	this.processedContent = function processedContent() {
+		return this.translation.toLowerCase();
+	}
+}
+
+TranslatedPhrase.prototype = new Phrase();
